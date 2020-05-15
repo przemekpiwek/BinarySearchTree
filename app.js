ecb3bfb13c4e343123de1c2a5910c97a4ebfd641
Przemek Piwek <przemekpiwek@gmail.com>
2020-05-14 21:03:37 -0600

    RemoveNode, no root or visuals

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ class Node {
     this.positionX = null;
     this.positionY = null;
   }
-  createDom(parent) {
+  createDom() {
     let nodeEl = document.createElement("inline-block");
     nodeEl.style.left = this.positionX;
     nodeEl.style.top = this.positionY;
     nodeEl.innerText = `${this.value}`;
+    nodeEl.id = this.value;
     nodeEl.classList.add("node");
     body.appendChild(nodeEl);
   }
@@ -32,33 +33,87 @@ class Tree {
   }
   addNode(value) {
     let newNode = new Node(value);
-    let parent;
+    let curNode;
 
     if (this.root === null) {
       this.root = newNode;
-      parent = newNode;
-      parent.positionX = `${window.innerWidth / 2 - 50}px`;
-      parent.positionY = "0px";
+      curNode = newNode;
+      curNode.positionX = `${window.innerWidth / 2 - 50}px`;
+      curNode.positionY = "0px";
     } else {
-      let curNode = this.root;
-
-      while (curNode) {
-        parent = curNode;
-        curNode = curNode.value > newNode.value ? curNode.left : curNode.right;
-      }
-      if (parent.value > newNode.value) {
-        parent.left = newNode;
-        newNode.positionX = `${parseInt(parent.positionX, 10) - 100}px`;
-        newNode.positionY = `${parseInt(parent.positionY, 10) + 100}px`;
-      } else if (parent.value < newNode.value) {
-        parent.right = newNode;
-        newNode.positionX = `${parseInt(parent.positionX, 10) + 100}px`;
-        newNode.positionY = `${parseInt(parent.positionY, 10) + 100}px`;
+      curNode = this.searchForNode(value)["curNode"];
+      if (curNode.value > newNode.value) {
+        curNode.left = newNode;
+        newNode.positionX = `${parseInt(curNode.positionX, 10) - 100}px`;
+        newNode.positionY = `${parseInt(curNode.positionY, 10) + 100}px`;
+      } else {
+        curNode.right = newNode;
+        newNode.positionX = `${parseInt(curNode.positionX, 10) + 100}px`;
+        newNode.positionY = `${parseInt(curNode.positionY, 10) + 100}px`;
       }
     }
     newNode.createDom();
-    return newNode;
   }
+  removeNode(value) {
+    const { curNode, parent } = this.searchForNode(value);
+    const nodeToRemove = curNode;
+    const removedNodeChildren = this.mergeSubBranch(nodeToRemove);
+    const nodeDom = document.getElementById(`${nodeToRemove.value}`);
+    console.log(parent);
+    console.log(nodeDom);
+    console.log(nodeToRemove);
+
+    if (!nodeToRemove) {
+      console.log("Node not found");
+    } else if (value === this.root.value) {
+      console.log("to be done");
+    } else {
+      if (parent.right) {
+        if (parent.right.value === nodeToRemove.value) {
+          parent.right = removedNodeChildren;
+        }
+      } else {
+        parent.left = removedNodeChildren;
+      }
+    }
+
+    body.removeChild(nodeDom);
+  }
+  searchForNode(value) {
+    let testNode = this.root;
+    let curNode;
+    let parent;
+
+    while (testNode) {
+      parent = curNode;
+      curNode = testNode;
+      testNode = testNode.value > value ? testNode.left : testNode.right;
+    }
+    return { curNode, parent };
+  }
+  mergeSubBranch(node) {
+    if (node.right) {
+      let leftestNode = this.mostLeft(node.right);
+      leftestNode.left = node.left;
+      return node.right;
+    } else {
+      return node.left;
+    }
+  }
+  mostLeft(node) {
+    if (!node.left) {
+      return node;
+    } else {
+      let curNode = node;
+      let lastNode;
+      while (curNode) {
+        lastNode = curNode;
+        curNode = lastNode.left;
+      }
+      return lastNode;
+    }
+  }
+  adjustPosition() {}
 }
 
 let tree = new Tree();
